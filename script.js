f0fb2d4cf4e10cfcadaef25678edf4a704c2932e
Dima <fgjd.gh50@gmail.com>
2024-10-19 18:43:35 +0300

    update UI

--- script.js
+++ script.js
@@ -36,6 +36,7 @@ $(document).ready(function () {
     ],
   });
 });
+
 function toggleMenu() {
   const navList = document.getElementById('navList');
   const iconMenu = document.querySelector('.icon-menu');
